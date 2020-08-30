46bd349b54735ec378125526bcc4bbb01e3cfdb5
Patrick Tomas <p.tomas14@gmail.com>
2020-08-29 20:22:46 -0700

    input message

--- function.js
+++ function.js
@@ -1,89 +1,157 @@
-var a = document.getElementById("a");
-var b = document.getElementById("b");
-var c = document.getElementById("c");
+var a = document.getElementById("questionA");
+var b = document.getElementById("questionB");
+var c = document.getElementById("questionC");
+var d = document.getElementById("questionD");
+var e = document.getElementById("questionE");
+
 
 var score;
-var counter =0;
+var counter = 0;
+var correct1;
+var correct2;
+var correct3;
+var correct4;
 
+
+
+      $("questionB").detach();
+      $("questionC").detach();
+      $("questionD").detach();
+      $("questionE").detach();
 function startFunction() {
-    //var x = document.getElementById("jumboLink");
-    if (a.style.display === "block") {
-      a.style.display = "block";
-      b.style.display = "none";
-      c.style.display = "none";
-    } else {
-      b.style.display = "block";
-      a.style.display = "none";
-      c.style.display = "none";
+
+  var startMinutes = 20;
+  var time = startMinutes*60;
+
+  var countdownEl = document.getElementById("countdown")
+
+  setInterval(updateCountdown, 1000);
+
+    function updateCountdown() {
+      var min = Math.floor(time/60);
+      var seconds = time % 60;
+  
+      countdownEl.innerHTML = '${minutes}: ${seconds}';
+      time--;
     }
+
+    //var x = document.getElementById("jumboLink");
+    $("questionA").detach();
+
+    var paragraph= $("<p>");
+    paragraph.attr("style", "color:" + black);
+    $(".body").append(paragraph);
+    paragraph.text(b)
+    
 }
 
 document.getElementById("b").style.display = "none";
   function next1() {
-    //var y = document.getElementById("photoLink");
-    
-    if (b.style.display === "block") {
-        c.style.display = "none";
-        a.style.display = "none";
-        b.style.display = "block";
-    } else {
-        c.style.display = "block";
-        a.style.display = "none";
-        b.style.display = "none";
+
+    if(correct1==true){
+      counter++
+      localStorage.score= counter;
+      var paragraph= $("<p>");
+      paragraph.text("Score:"+ counter);
+      $(".header").append(paragraph);
     }
+
+    //var y = document.getElementById("photoLink");
+    $("questionB").detach();
+
+
+    var paragraph= $("<p>");
+    paragraph.attr("style", "color:" + black);
+    $(".body").append(paragraph);
+    paragraph.text(c)
   }
 
   document.getElementById("c").style.display = "none";
-  function photoFunction() {
-    //var y = document.getElementById("photoLink");
-    
-    if (c.style.display === "block") {
-        c.style.display = "block";
-        a.style.display = "none";
-        b.style.display = "none";
-    } else {
-        c.style.display = "block";
-        a.style.display = "none";
-        b.style.display = "none";
+  function next2() {
+
+    if(correct1==true){
+      counter++
+      localStorage.score= counter;
+      var paragraph= $("<p>");
+      paragraph.text("Score:"+ counter);
+      $(".header").append(paragraph);
     }
-  }
 
-  function startFunction(){
+    //var y = document.getElementById("photoLink");
+    $("questionC").detach();
+
+
     var paragraph= $("<p>");
-    paragraph.text("Score:"+ counter);
-    $(".header").append(paragraph);
+    paragraph.attr("style", "color:" + black);
+    $(".body").append(paragraph);
+    paragraph.text(d)
+    
   }
 
-  function counterUp(){
-    counter++
-    console.log(counter)
-}
+  document.getElementById("d").style.display = "none";
+  function next3() {
+    //var y = document.getElementById("photoLink");
+    $("questionD").detach();
 
-  function next1(){
-      if (counter>=1){
-        localStorage.score= 1;
-        console.log(localStorage)
-      }
+
+    var paragraph= $("<p>");
+    paragraph.attr("style", "color:" + black);
+    $(".body").append(paragraph);
+    paragraph.text(e)
   }
 
-function startFunction(){
-  var seconds = 0;
-  var interval ;
-  function timer(mins) {
-      seconds = mins*60 || 0;     
-      interval = setInterval(function() {
+  document.getElementById("e").style.display = "none";
+  function next4() {
+    //var y = document.getElementById("photoLink");
+    $("questionE").detach();
 
-      seconds--;
-      if(!seconds){
-       clearInterval(interval); 
-       alert("times up");
-      }
-      },1000)
+    
+    var paragraph= $("<p>");
+    paragraph.attr("style", "color:" + black);
+    $(".body").append(paragraph);
+    paragraph.text(e)
   }
 
-  var time = $("<p>");
-  time.text(interval);
-  $(".header").append(time);
+var correctAnswer = document.querySelector("input[name=exampleRadios1]");
+correctAnswer.addEventListener("change", score1);
+
+  function score1(){
+    correct1 == true;
+    console.log(correct1);
+    
+    
+  }
+
+  var correctAnswer = document.querySelector("input[name=exampleRadios3]");
+correctAnswer.addEventListener("change", score2);
+
+  function score2(){
+    correct2 == true;
+    console.log(correct2);
+    
+    
+  }
+
+  var correctAnswer = document.querySelector("input[name=exampleRadios4]");
+correctAnswer.addEventListener("change", score3);
+
+  function score3(){
+    correct3 == true;
+    console.log(correct3);
+    
+    
+  }
+
+  var correctAnswer = document.querySelector("input[name=exampleRadios]");
+correctAnswer.addEventListener("change", score4);
+
+  function score4(){
+    correct4 == true;
+    console.log(correct4);
+    
+    
+  }
   
-}
+
+
   
